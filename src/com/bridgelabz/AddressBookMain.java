6bc20583165b8c64890998444d6bc81518a42773
iamtalib13 <talibsheikh16@gmail.com>
2022-08-17 13:49:57 +0530

    Added - Ability to Create a Contact in AddressBook

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -3,5 +3,7 @@ package com.bridgelabz;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        Contact contact=new Contact();
+        contact.setContactDetails();
     }
 }
---
+++ src/com/bridgelabz/Contact.java
@@ -0,0 +1,96 @@
+package com.bridgelabz;
+import java .util.Scanner;
+public class Contact {
+    public String firstName;
+    public String lastName;
+    public String address;
+    public String city;
+    public String state;
+    public String zipcode;
+    public String phoneNo;
+    public String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipcode;
+    }
+
+    public void setZipCode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    Scanner sc = new Scanner(System.in);
+    public void setContactDetails() {
+        System.out.print(" Enter First Name : ");
+        setFirstName(sc.nextLine());
+        System.out.print(" Enter Last Name : ");
+        setLastName(sc.nextLine());
+        System.out.print(" Enter Address : ");
+        setAddress(sc.nextLine());
+        System.out.print(" Enter city : ");
+        setCity(sc.nextLine());
+        System.out.print(" Enter State : ");
+        setState(sc.nextLine());
+        System.out.print(" Enter Zip Code : ");
+        setZipCode(sc.nextLine());
+        System.out.print(" Enter Phone Number : ");
+        setPhoneNo(sc.nextLine());
+        System.out.print(" Enter Email : ");
+        setEmail(sc.nextLine());
+    }
+}
