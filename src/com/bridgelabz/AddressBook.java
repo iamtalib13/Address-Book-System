59965923a4d9bb2bef54c49d5ccf3e765f7742b8
iamtalib13 <talibsheikh16@gmail.com>
2022-08-18 21:27:41 +0530

    Added - ability to Edit Existing Contact Details using their name

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -4,28 +4,36 @@ import java.util.Scanner;
 
 public class AddressBook {
     Scanner sc = new Scanner(System.in);
+    Scanner sc1 = new Scanner(System.in);
+    Scanner sc2 = new Scanner(System.in);
+    Scanner sc3 = new Scanner(System.in);
+    Scanner sc4 = new Scanner(System.in);
+    Scanner sc5 = new Scanner(System.in);
+    Scanner sc6 = new Scanner(System.in);
+    Scanner sc7 = new Scanner(System.in);
+    Scanner sc8 = new Scanner(System.in);
     Contact contacts = new Contact();
 
     public void addNewContact() {
         System.out.println("Enter the First Name :");
-        contacts.setFirstName(sc.next());
+        contacts.setFirstName(sc1.next());
         System.out.println("Enter the Last Name :");
-        contacts.setLastName(sc.next());
+        contacts.setLastName(sc2.next());
         System.out.println("Enter the Address :");
-        contacts.setAddress(sc.next());
+        contacts.setAddress(sc3.next());
         System.out.println("Enter the City :");
-        contacts.setCity(sc.next());
+        contacts.setCity(sc4.next());
         System.out.println("Enter the State :");
-        contacts.setState(sc.next());
+        contacts.setState(sc5.next());
         System.out.println("Enter the Zip Code :");
-        contacts.setZipCode(sc.next());
+        contacts.setZipCode(sc6.next());
         System.out.println("Enter the Phone Number :");
-        contacts.setPhoneNo(sc.next());
+        contacts.setPhoneNo(sc7.next());
         System.out.println("Enter the EMail ID :");
-        contacts.setEmail(sc.next());
+        contacts.setEmail(sc8.next());
     }
 
-    public void ShowContact() {
+    public void showContact() {
         System.out.println("First Name : " + contacts.getFirstName());
         System.out.println("Last Name : " + contacts.getLastName());
         System.out.println("Address : " + contacts.getAddress());
@@ -34,5 +42,30 @@ public class AddressBook {
         System.out.println("Zip Code : " + contacts.getZipCode());
         System.out.println("Phone Number : " + contacts.getPhoneNo());
         System.out.println("EMail ID : " + contacts.getEmail());
+    }
+
+    public void editContact() {
+        System.out.println("Enter the First Name : ");
+        String firstName = sc1.next();
+
+        if (!firstName.equalsIgnoreCase(contacts.getFirstName())) {
+            System.out.println("The Entered Contact Name is Not Available in Address Book");
+        } else {
+
+            System.out.println("Enter the Last Name :");
+            contacts.setLastName(sc2.next());
+            System.out.println("Enter the Address :");
+            contacts.setAddress(sc3.next());
+            System.out.println("Enter the City :");
+            contacts.setCity(sc4.next());
+            System.out.println("Enter the State :");
+            contacts.setState(sc5.next());
+            System.out.println("Enter the Zip Code :");
+            contacts.setZipCode(sc6.next());
+            System.out.println("Enter the Phone Number :");
+            contacts.setPhoneNo(sc7.next());
+            System.out.println("Enter the EMail ID :");
+            contacts.setEmail(sc8.next());
+        }
     }
 }
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -12,7 +12,8 @@ public class AddressBookMain {
         do {
             System.out.println("1. ADD NEW PERSON DETAILS");
             System.out.println("2. SHOW PERSON DETAILS");
-            System.out.println("3. EXIT ADDRESS BOOK");
+            System.out.println("3. EDIT PERSON DETAILS");
+            System.out.println("4. EXIT ADDRESS BOOK");
 
             System.out.println("Please Select the Program : ");
             option = addressBook.sc.nextInt();
@@ -22,16 +23,19 @@ public class AddressBookMain {
                     addressBook.addNewContact();
                     break;
                 case 2:
-                    addressBook.ShowContact();
+                    addressBook.showContact();
                     break;
                 case 3:
+                    addressBook.editContact();
+                    break;
+                case 4:
                     System.out.println("Exit - Thank You ");
                     break;
                 default:
                     System.out.println("Please Select the Valid Program");
                     break;
             }
-        } while (option != 3);
+        } while (option != 4);
 
     }
 }
